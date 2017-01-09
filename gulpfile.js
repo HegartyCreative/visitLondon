cc5060ae78b1f39ddd055d90bc60789a7130c7e0
Sean <sean.hegarty@btinternet.com>
2017-01-09 20:43:43 +0000

    Added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,36 @@
 var gulp = require('gulp');
-var sass    = require('gulp-sass');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var sourcePaths = {
+    sassSource: 'src/scss/*.scss'
+};
+
+var appPaths = {
+    root: 'app/',
+    css: 'app/css/',
+    js: 'app/js'
+};
 
 
 gulp.task('sass', function(){
-    return gulp.src('src/scss/app.scss')
+    return gulp.src(sourcePaths.sassSource)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('appPaths.css'));
+});
+
+gulp.task('watch', ['serve', 'sass'], function(){
+    gulp.watch([sourcePaths.sassSource], ['sass'])
+});
+
+gulp.task('serve', ['sass'], function(){
+    browserSync.init([appPaths.css + '/*.css', appPaths.root + '/*.html', appPaths.js + '/*.js'],{
+        server: {
+            baseDir: appPaths.root
+        }
+    })
 });
 
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['watch']);
