b8fbfbccfffd3148cce260992ffba31a18af9317
Sean <sean.hegarty@btinternet.com>
2017-01-10 19:58:53 +0000

    Added clean task for js and html files

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,29 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
 
 var sourcePaths = {
     sassSource: 'src/scss/*.scss',
-    htmlSource: 'src/*.html'
+    htmlSource: 'src/*.html',
+    jsSource: 'src/js/**'
 };
 
 var appPaths = {
     root: 'app/',
     css: 'app/css/',
-    js: 'app/js'
+    js: 'app/js/'
 };
 
+gulp.task('clean-html', function(){
+    return gulp.src(appPaths.root + '*.html', {read: false, force: true})
+    .pipe(clean());
+});
+
+gulp.task('clean-scripts', function(){
+    return gulp.src(appPaths.js + '*.js', {read: false, force: true})
+    .pipe(clean());
+});
 
 gulp.task('sass', function(){
     return gulp.src(sourcePaths.sassSource)
@@ -23,7 +34,12 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(appPaths.css));
 });
 
-gulp.task('copy', function(){
+gulp.task('scripts', ['clean-scripts'], function(){
+    gulp.src(sourcePaths.jsSource)
+    .pipe(gulp.dest(appPaths.js))
+});
+
+gulp.task('copy', ['clean-html'], function(){
     gulp.src(sourcePaths.htmlSource)
     .pipe(gulp.dest(appPaths.root))
 });
@@ -36,9 +52,10 @@ gulp.task('serve', ['sass'], function(){
     })
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy'], function(){
+gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts'], function(){
     gulp.watch([sourcePaths.sassSource], ['sass']);
     gulp.watch([sourcePaths.htmlSource], ['copy']);
+    gulp.watch([sourcePaths.jsSource], ['scripts']);
 });
 
 gulp.task('default', ['watch']);
