2b69e4dd4bafc72998debde342bcf6890363f385
Sean <sean.hegarty@btinternet.com>
2017-01-11 12:54:04 +0000

    Added partials and minification of JS files

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,15 @@ var browserify = require ('gulp-browserify');
 var merge = require ('merge-stream');
 var newer = require ('gulp-newer');
 var imagemin = require ('gulp-imagemin');
+var injectPartials = require ('gulp-inject-partials');
+var minify = require ('gulp-minify');
+var rename = require ('gulp-rename');
+var cssmin = require ('gulp-cssmin');
 
 var sourcePaths = {
     sassSource: 'src/scss/*.scss',
     htmlSource: 'src/*.html',
+    htmlPartial: 'src/partial/*.html',
     jsSource: 'src/js/**',
     imgSource: 'src/images/**'
 };
@@ -66,10 +71,44 @@ gulp.task('scripts', ['clean-scripts'], function(){
     .pipe(gulp.dest(appPaths.js))
 });
 
+/** Production Tasks **/
+
+gulp.task('compress', function(){
+    gulp.src(sourcePaths.jsSource)
+    .pipe(concat('main.js'))
+    .pipe(browserify())
+    .pipe(minify())
+    .pipe(gulp.dest(appPaths.js))
+});
+
+gulp.task('compresscss', function(){
+    var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+    var sassFiles;
+    sassFiles = gulp.src(sourcePaths.sassSource)
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    return merge( bootstrapCSS, sassFiles)
+    .pipe(concat('app.css'))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(appPaths.css));
+});
+
+/** End of Production Tasks **/
+
+gulp.task('html', function(){
+    return gulp.src(sourcePaths.htmlSource)
+    .pipe(injectPartials())
+    .pipe(gulp.dest(appPaths.root))
+});
+
+
+/*
 gulp.task('copy', ['clean-html'], function(){
     gulp.src(sourcePaths.htmlSource)
     .pipe(gulp.dest(appPaths.root))
 });
+*/
 
 gulp.task('serve', ['sass'], function(){
     browserSync.init([appPaths.css + '/*.css', appPaths.root + '/*.html', appPaths.js + '/*.js'],{
@@ -79,11 +118,12 @@ gulp.task('serve', ['sass'], function(){
     })
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts', 'images'], function(){
+gulp.task('watch', ['serve', 'sass', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts', 'html', 'images'], function(){
     gulp.watch([sourcePaths.sassSource], ['sass']);
-    gulp.watch([sourcePaths.htmlSource], ['copy']);
+    //gulp.watch([sourcePaths.htmlSource], ['copy']);
     gulp.watch([sourcePaths.jsSource], ['scripts']);
     gulp.watch([sourcePaths.imgSource], ['images']);
+    gulp.watch([sourcePaths.htmlSource, sourcePaths.htmlPartial], ['html']);
 });
 
 gulp.task('default', ['watch']);
